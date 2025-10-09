82522c6504f72d93514889760404e995321c134e
zeiandev <severity@gmail.com>
2025-10-09 16:13:27 +0800

    .

--- src/app/classes/page.tsx
+++ src/app/classes/page.tsx
@@ -21,4 +21,3 @@ export default async function ClassesPage() {
 
   return <ClassesClient sections={sections} initialSectionId={activeSectionId} initialClasses={(classesData ?? []) as ClassRow[]} />;
 }
-s
